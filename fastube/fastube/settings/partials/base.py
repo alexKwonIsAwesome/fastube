794222def68bb19be25def4f92cf58658cce1ffe
alexKwonIsAwesome <khwalex@gmail.com>
2016-06-21 16:21:39 +0900

    updated signup page: seperated base html and its header, footer

--- fastube/fastube/settings/partials/base.py
+++ fastube/fastube/settings/partials/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 PROJECT_ROOT_DIR = os.path.dirname(BASE_DIR)
 
 
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'fastube.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "fastube", "templates"),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
