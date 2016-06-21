eae6b6b286e090165b4078d401d2df9ce532cb28
alexKwonIsAwesome <khwalex@gmail.com>
2016-06-22 08:38:14 +0900

    init signup view: render into signup.html
    added users app in application,
    revised baseroot directory in settings
    added url patterns,

---
+++ fastube/users/views/auth/signup.py
@@ -0,0 +1,13 @@
+from django.views.generic import View
+from django.shortcuts import render
+
+
+class SignupView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(
+                request,
+                "auth/signup.html",
+                {},
+        )
+
