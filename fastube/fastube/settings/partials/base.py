09d328928be769232d80cb3281d2d5dea62fbe6e
alexKwonIsAwesome <khwalex@gmail.com>
2016-06-21 09:27:13 +0900

    init users application

--- fastube/fastube/settings/partials/base.py
+++ fastube/fastube/settings/partials/base.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+
+    'users',
+
+    ]
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
@@ -50,7 +53,6 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'fastube.urls'
 
 TEMPLATES = [
